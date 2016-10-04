c0ac9e877cc6b8dfdf1822eee194eba8977dcf57
Thomas Wiringa <thomas.wiringa@gmail.com>
2016-10-04 16:40:09 +0200

    Fix env vars not being used correctly

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,38 @@
 #!/bin/sh
 
-local domain=${$FICHE_DOMAIN:-'http://localhost/'}
-local port=${$FICHE_PORT:-9999}
-local basedir=${$FICHE_BASEDIR:-'/app/uploads/'}
+set -e
 
-/app/fiche -d "$domain" -p "$port" -o "$basedir"
+local domain
+local port
+local basedir
+local user
+
+if [ -n "$FICHE_DOMAIN" ]; then
+    domain="$FICHE_DOMAIN"
+else
+    domain="http://localhost/"
+fi
+
+if [ -n "$FICHE_PORT" ]; then
+    port="$FICHE_PORT"
+else
+    port="9999"
+fi
+
+if [ -n "$FICHE_BASEDIR" ]; then
+    basedir="$FICHE_BASEDIR"
+else
+    basedir="/app/uploads"
+fi
+
+if [ -n "$FICHE_USER" ]; then
+    user="$FICHE_USER"
+else
+    user="root"
+fi
+
+/app/fiche \
+    -d "$domain" \
+    -p "$port" \
+    -o "$basedir" \
+    -u "$user"
